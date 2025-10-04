fd0745370e7ec5d455fb1b8afa1f4e02793e9001
Fernando Ismael Navarrete Ruiz <fernandonr189@outlook.com>
2025-10-03 21:45:22 -0600

    Basi ui setup

---
+++ lib/screens/HomeScreen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _HomeScreenState();
+  }
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+
+        title: Text("Home screen"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('You have pushed the button this many times:'),
+          ],
+        ),
+      ),
+    );
+  }
+}
